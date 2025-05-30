0f40e935825c5d65102c710c3e060fa2752586ea
divinekatende <divinekatende23@gmail.com>
2025-05-30 15:40:10 +0100

    bouton

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-const menuBtn = document.getElementById('menuBtn');
-const menuNav = document.getElementById('menuNav');
 
-menuBtn.addEventListener('click', () => {
-  menuNav.classList.toggle('open');
-});
+<script>
+  const menuBtn = document.getElementById('menuBtn');
+  const menuNav = document.getElementById('menuNav');
+
+  menuBtn.addEventListener('click', () => {
+    menuNav.classList.toggle('open');
+  });
+</script>
