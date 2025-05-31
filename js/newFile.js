4ec9f19d64c1bee0433b489ca09d058459879745
divinekatende <divinekatende23@gmail.com>
2025-05-31 12:11:53 +0100

    amelioration du bouton

--- js/newFile.js
+++ js/newFile.js
@@ -1,9 +1,9 @@
-
 <script>
   const menuBtn = document.getElementById('menuBtn');
   const menuNav = document.getElementById('menuNav');
 
+  // Quand on clique sur le bouton, on ajoute ou enlève la classe 'active'
   menuBtn.addEventListener('click', () => {
-    menuNav.classList.toggle('open');
+    menuNav.classList.toggle('active');
   });
 </script>
